975fbd698a8bdcccd62b5d70a89ca75a0c8905d1
prakash shelke <avikdigidev@gmail.com>
2024-03-31 01:50:46 +0530

    Getter and setter

--- order-service/src/main/java/com/ecommerce/orderservice/entity/Order.java
+++ order-service/src/main/java/com/ecommerce/orderservice/entity/Order.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
     @Id
     @GeneratedValue
@@ -19,43 +25,4 @@ public class Order {
     @Column
     private String userId;
 
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
--- order-service/src/main/java/com/ecommerce/orderservice/entity/Products.java
+++ order-service/src/main/java/com/ecommerce/orderservice/entity/Products.java
@@ -3,98 +3,30 @@ package com.ecommerce.orderservice.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @Entity
 public class Products {
- @Id
- private String productId;
-  @Column
-   private String productName;
-  @Column
-   private String category;
-  @Column
-   private Double price;
-  @Column
-   private LocalDate date;
-  @Column
-   private Double rating;
-  @Column
-   private Integer stock;
+    @Id
+    private String productId;
+    @Column
+    private String productName;
+    @Column
+    private String category;
+    @Column
+    private Double price;
+    @Column
+    private LocalDate date;
+    @Column
+    private Double rating;
+    @Column
+    private Integer stock;
 
- public String getProductId() {
-  return productId;
- }
-
- public void setProductId(String productId) {
-  this.productId = productId;
- }
-
- public String getProductName() {
-  return productName;
- }
-
- public void setProductName(String productName) {
-  this.productName = productName;
- }
-
- public String getCategory() {
-  return category;
- }
-
- public void setCategory(String category) {
-  this.category = category;
- }
-
- public Double getPrice() {
-  return price;
- }
-
- public void setPrice(Double price) {
-  this.price = price;
- }
-
- public LocalDate getDate() {
-  return date;
- }
-
- public void setDate(LocalDate date) {
-  this.date = date;
- }
-
- public Double getRating() {
-  return rating;
- }
-
- public void setRating(Double rating) {
-  this.rating = rating;
- }
-
- public Integer getStock() {
-  return stock;
- }
-
- public void setStock(Integer stock) {
-  this.stock = stock;
- }
-
- @Override
- public String toString() {
-  return "Products{" +
-          "productId=" + productId +
-          ", productName='" + productName + '\'' +
-          ", category='" + category + '\'' +
-          ", price=" + price +
-          ", date=" + date +
-          ", rating=" + rating +
-          ", inventory=" + stock +
-          '}';
- }
 }
